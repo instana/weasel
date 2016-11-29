8765c4c37c721b0789602c3b6f466d73a486028f
Ben Ripkens <bripkens.dev@gmail.com>
2016-11-29 17:29:56 +0100

    How about some commit hooks

--- __tests__/util-test.js
+++ __tests__/util-test.js
@@ -4,8 +4,7 @@ describe('util', () => {
   describe('generateUniqueId', () => {
     it('must stick to the zipkin trace id contract', () => {
       for (let i = 0; i < 500; i++) {
-        const id = generateUniqueId();
-        expect(id).toMatch(/^[0-9a-f]{16}$/i);
+        expect(generateUniqueId()).toMatch(/^[0-9a-f]{16}$/i);
       }
     });
   });

637cfa767b244a5eeb8c1158e33092dc92640985
Ben Ripkens <bripkens.dev@gmail.com>
2016-11-29 06:53:57 +0100

    Support debug and production builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,16 @@
+const replace = require('rollup-plugin-replace');
+const flow = require('rollup-plugin-flow');
+
+const isDebugBuild = process.env.NODE_ENV !== 'production';
+
 export default {
   entry: 'lib/index.js',
-  dest: 'target/weasel.js',
-  format: 'iife'
+  dest: `target/${process.env.FILENAME}.js`,
+  format: 'iife',
+  plugins:[
+    flow(),
+    replace({
+      DEBUG: JSON.stringify(isDebugBuild)
+    })
+  ]
 };

38a5d4560952356c8a5015e3dd5c9c1bbbdb7fc8
Ben Ripkens <bripkens.dev@gmail.com>
2016-12-02 19:55:16 +0100

    Use babel transformation for bundle build

--- flow-typed/globals.js
+++ flow-typed/globals.js
@@ -1,12 +1,1 @@
 declare var DEBUG: bool;
-
-declare type EumEvent = {
-  initialize: () => mixed,
-  name: string,
-  time: ?number
-}
-
-declare type EumState = {
-  name: string,
-  onEnter: () => mixed
-}
--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,9 @@
+/* eslint-env node */
+
 const replace = require('rollup-plugin-replace');
-const flow = require('rollup-plugin-flow');
+const babel = require('rollup-plugin-babel');
+const path = require('path');
+const fs = require('fs');
 
 const isDebugBuild = process.env.NODE_ENV !== 'production';
 
@@ -8,9 +12,21 @@ export default {
   dest: `target/${process.env.FILENAME}.js`,
   format: 'iife',
   plugins:[
-    flow(),
+    babel({
+      babelrc: false,
+      exclude: 'node_modules/**',
+      plugins: getPlugins()
+    }),
     replace({
       DEBUG: JSON.stringify(isDebugBuild)
     })
   ]
 };
+
+
+function getPlugins() {
+  const content = fs.readFileSync(path.join(__dirname, '.babelrc'), {encoding: 'utf8'});
+  const config = JSON.parse(content);
+  return config.plugins
+    .filter(plugin => plugin !== 'transform-es2015-modules-commonjs');
+}

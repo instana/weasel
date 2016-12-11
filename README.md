4a4c0201cf199d4f48b646783aeb88652656a806
Ben Ripkens <bripkens.dev@gmail.com>
2016-12-11 19:26:14 +0100

    Add travis badge

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Weasel
+# Weasel &nbsp; [![Build Status](https://travis-ci.org/bripkens/weasel.svg?branch=master)](https://travis-ci.org/bripkens/weasel)
+
+---
--- protractor.config.js
+++ protractor.config.js
@@ -1,8 +1,16 @@
 /* eslint-env node */
 
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumAddress: process.env.TRAVIS ? null : 'http://localhost:4444/wd/hub',
   specs: [
     'test/e2e/**/*.spec.js'
-  ]
+  ],
+
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  capabilities: {
+    browserName: 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    build: process.env.TRAVIS_JOB_NUMBER
+  }
 };

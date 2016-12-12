a02f1a36c492127e773a3d9efabd82287b85a66f
Ben Ripkens <bripkens.dev@gmail.com>
2016-12-12 09:34:10 +0100

    WebDriver log endpoint is not available for old IE instance, so remove this code as it is worthless

--- test/e2e/base.js
+++ test/e2e/base.js
@@ -1,18 +1,6 @@
-/* eslint-disable no-console */
-
-const util = require('util');
-
 exports.registerBaseHooks = () => {
   beforeEach(() => {
     // we are not using Angular.js
     browser.ignoreSynchronization = true;
-  });
-
-  afterEach(() => {
-    if (process.env.CI) {
-      browser.manage().logs().get('browser').then(function(browserLog) {
-        console.log('Browser logs: ' + util.inspect(browserLog));
-      });
-    }
   });
 };

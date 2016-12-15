1bc228645d1e0030e19007f3655d7c2f394e3243
Ben Ripkens <bripkens.dev@gmail.com>
2016-12-15 10:03:37 +0100

    Add navigation timing tests

--- test/e2e/00_pageLoad.spec.js
+++ test/e2e/00_pageLoad.spec.js
@@ -104,7 +104,47 @@ describe('00_pageLoad', () => {
     });
   });
 
-  // TODO test navigation timings
+  describe('00_navigationTimings', () => {
+    beforeEach(() => {
+      browser.get(getE2ETestBaseUrl('00_navigationTimings'));
+    });
+
+    it('must send navigation timings', () => {
+      return util.retry(() => {
+        return getBeacons()
+          .then(([beacon]) => {
+            testIsPositiveInteger(beacon.t_unl);
+            testIsPositiveInteger(beacon.t_red);
+            testIsPositiveInteger(beacon.t_apc);
+            testIsPositiveInteger(beacon.t_dns);
+            testIsPositiveInteger(beacon.t_tcp);
+            testIsPositiveInteger(beacon.t_req);
+            testIsPositiveInteger(beacon.t_rsp);
+            testIsPositiveInteger(beacon.t_pro);
+            testIsPositiveInteger(beacon.t_loa);
+          });
+      });
+    });
+
+    it('must send either no first paint time or a time > 0', () => {
+      return util.retry(() => {
+        return getBeacons()
+          .then(([beacon]) => {
+            // ensure that we have a beacon with some data
+            testIsPositiveInteger(beacon.t_loa);
+
+            if (beacon.t_fp !== undefined) {
+              testIsPositiveInteger(beacon.t_fp, 1);
+            }
+          });
+      });
+    });
+
+    function testIsPositiveInteger(s, minInclusive = 0) {
+      cexpect(s).to.match(/^\d+$/);
+      cexpect(parseInt(s, 10)).to.be.at.least(minInclusive);
+    }
+  });
 
   describe('00_resourceTimings', () => {
     beforeEach(() => {
@@ -166,7 +206,6 @@ describe('00_pageLoad', () => {
       return hasResourceTimingSupport(capabilities) &&
         capabilities.browserName !== 'internet explorer' &&
         capabilities.browserName !== 'MicrosoftEdge';
-
     }
   });
 });

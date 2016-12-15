99999c26bb54a7ec33888a8035e77f2b93624d93
Ben Ripkens <bripkens.dev@gmail.com>
2016-12-15 10:06:42 +0100

    Test against more browsers

--- protractor.config.js
+++ protractor.config.js
@@ -8,15 +8,14 @@ if (process.env.TRAVIS) {
     sauceBuild: process.env.TRAVIS_JOB_NUMBER,
     multiCapabilities: [
       newSaucelabsCapability('chrome', '54.0', 'OS X 10.11'),
+      newSaucelabsCapability('chrome', '48.0', 'Windows 10'),
+      newSaucelabsCapability('firefox', '45.0', 'Linux'),
       newSaucelabsCapability('internet explorer', '11.103', 'Windows 10'),
+      newSaucelabsCapability('internet explorer', '9.0', 'Windows 7'),
       newSaucelabsCapability('internet explorer', '8.0', 'Windows 7'),
       newSaucelabsCapability('internet explorer', '6.0', 'Windows XP'),
-      newSaucelabsCapability('firefox', '45.0', 'Linux'),
       newSaucelabsCapability('safari', '10.0', 'OS X 10.11'),
       newSaucelabsCapability('safari', '9.0', 'OS X 10.11'),
-      // Saucelabs is currently lacking for Sauce connect for MicrosoftEdge.
-      // This means, that tests will always fail.
-      // - Ben 2016-12-13
       newSaucelabsCapability('MicrosoftEdge', '14.14393', 'Windows 10')
     ],
     // Do not allow parallel test execution. Makes the test execution a lot

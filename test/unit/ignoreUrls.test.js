acca1c82d5cf9372b0e95eaf1cd707785ebbcd91
Ben Ripkens <bripkens@gmail.com>
2017-09-08 07:01:15 +0200

    Ignore ping like URLs by default

--- test/unit/ignoreUrls.test.js
+++ test/unit/ignoreUrls.test.js
@@ -6,6 +6,7 @@ import vars from '../../lib/vars';
 describe('ignoreUrls', () => {
   afterEach(() => {
     vars.ignoreUrls = [];
+    vars.ignorePings = true;
   });
 
   it('must not ignore URLs when the ignore list is empty', () => {
@@ -19,5 +20,23 @@ describe('ignoreUrls', () => {
     expect(isUrlIgnored('http://shop.example.com')).to.equal(true);
     expect(isUrlIgnored('http://example.comm')).to.equal(false);
     expect(isUrlIgnored('http://google.com')).to.equal(false);
+  });
+
+  it('must ignore pings by default', () => {
+    expect(isUrlIgnored('http://example.com/ping')).to.equal(true);
+    expect(isUrlIgnored('http://example.com/ping/')).to.equal(true);
+    expect(isUrlIgnored('http://example.com/dsadsadad/ping/')).to.equal(true);
+    expect(isUrlIgnored('http://ping.com/shop')).to.equal(false);
+    expect(isUrlIgnored('http://example.com/ping/example')).to.equal(false);
+    expect(isUrlIgnored('http://example.com/pingalistic')).to.equal(false);
+  });
+
+  it('must allow pings when the user knows what they are doing', () => {
+    vars.ignorePings = false;
+
+    expect(isUrlIgnored('http://example.com/ping')).to.equal(false);
+    expect(isUrlIgnored('http://example.com/ping/')).to.equal(false);
+    expect(isUrlIgnored('http://example.com/dsadsadad/ping/')).to.equal(false);
+    expect(isUrlIgnored('http://ping.com/shop')).to.equal(false);
   });
 });

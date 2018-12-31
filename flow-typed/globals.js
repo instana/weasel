4814897d95da9d075b680b7afdcb1d44db9511b5
Ben Ripkens <bripkens@gmail.com>
2018-12-31 10:55:20 +0100

    Support custom event reporting

--- flow-typed/globals.js
+++ flow-typed/globals.js
@@ -2,5 +2,9 @@ declare var DEBUG: bool;
 
 declare type ErrorLike = {
   message: string,
-  stack: ?string
+  stack: ?string,
+
+  // to permit ['foo'] based access that doesn't break
+  // when pushed through the Closure compiler
+  [key: string]: ?number|?string
 }

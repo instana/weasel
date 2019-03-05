f39eb635a4c6b24ff5a24b577c7afd007ea707d1
Ben Ripkens <bripkens@gmail.com>
2019-03-05 17:06:43 +0100

    Transmit page change beacons

---
+++ test/e2e/10_pageChanges/pageChanges.spec.js
@@ -0,0 +1,76 @@
+const {registerTestServerHooks, getE2ETestBaseUrl, getBeacons} = require('../../server/controls');
+const {registerBaseHooks} = require('../base');
+const {retry, expectOneMatching} = require('../../util');
+
+const cexpect = require('chai').expect;
+
+describe('10_pageChanges', () => {
+  registerTestServerHooks();
+  registerBaseHooks();
+
+  describe('pageChanges', () => {
+    beforeEach(() => {
+      browser.get(getE2ETestBaseUrl('10_pageChanges/pageChanges'));
+    });
+
+    it('must send page change events', () => {
+      return retry(async () => {
+        const beacons = await getBeacons();
+
+        const pageLoadBeacon = expectOneMatching(beacons, beacon => {
+          cexpect(beacon.ty).to.equal('pl');
+        });
+
+        expectOneMatching(beacons, beacon => {
+          cexpect(beacon.ty).to.equal('pc');
+          cexpect(beacon.ts).to.be.a('string');
+          cexpect(beacon.p).to.equal('first');
+          cexpect(beacon.pl).to.equal(pageLoadBeacon.t);
+        });
+
+        expectOneMatching(beacons, beacon => {
+          cexpect(beacon.ty).to.equal('pc');
+          cexpect(beacon.ts).to.be.a('string');
+          cexpect(beacon.p).to.equal('second');
+          cexpect(beacon.pl).to.equal(pageLoadBeacon.t);
+        });
+
+        cexpect(beacons.filter(b => b.p === 'first' && b.ty === 'pc')).to.have.lengthOf(1);
+        cexpect(beacons.filter(b => b.p === 'second' && b.ty === 'pc')).to.have.lengthOf(1);
+      });
+    });
+  });
+
+  describe('changingBackToPreviousPage', () => {
+    beforeEach(() => {
+      browser.get(getE2ETestBaseUrl('10_pageChanges/changingBackToPreviousPage'));
+    });
+
+    it('must send page change events', () => {
+      return retry(async () => {
+        const beacons = await getBeacons();
+
+        const pageLoadBeacon = expectOneMatching(beacons, beacon => {
+          cexpect(beacon.ty).to.equal('pl');
+        });
+
+        expectOneMatching(beacons, beacon => {
+          cexpect(beacon.ty).to.equal('pc');
+          cexpect(beacon.ts).to.be.a('string');
+          cexpect(beacon.p).to.equal('first');
+          cexpect(beacon.pl).to.equal(pageLoadBeacon.t);
+        });
+
+        expectOneMatching(beacons, beacon => {
+          cexpect(beacon.ty).to.equal('pc');
+          cexpect(beacon.ts).to.be.a('string');
+          cexpect(beacon.p).to.equal('second');
+          cexpect(beacon.pl).to.equal(pageLoadBeacon.t);
+        });
+
+        cexpect(beacons.filter(b => b.p === 'first' && b.ty === 'pc')).to.have.lengthOf(2);
+        cexpect(beacons.filter(b => b.p === 'second' && b.ty === 'pc')).to.have.lengthOf(1);
+      });
+    });
+  });
+});

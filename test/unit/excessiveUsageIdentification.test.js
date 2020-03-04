dda435334c1373611bb9284976b90e3197b7abe0
Ben Blackmore <bripkens@gmail.com>
2020-03-04 11:26:35 +0100

    Rework abuse protection (#48)
    **Why**
    
    In order to support long lived single-page apps.
    
    **What**
    
    Increase `maxCalls` and introduce a ten minute limiting.

--- test/unit/excessiveUsageIdentification.test.js
+++ test/unit/excessiveUsageIdentification.test.js
@@ -13,7 +13,11 @@ describe('excessiveUsageIdentification', () => {
 
   it('must identify excessive usage once the maximum call count is eached', () => {
     const maxCalls = 8;
-    isExcessiveUsage = createExcessiveUsageIdentifier({ maxCalls });
+    isExcessiveUsage = createExcessiveUsageIdentifier({
+      maxCalls,
+      maxCallsPerTenMinutes: Number.MAX_VALUE,
+      maxCallsPerTenSeconds: Number.MAX_VALUE
+    });
 
     for (let i = 0; i < maxCalls; i++) {
       expect(isExcessiveUsage()).to.equal(false);
@@ -23,20 +27,41 @@ describe('excessiveUsageIdentification', () => {
     expect(isExcessiveUsage()).to.equal(true);
   });
 
-  it('must identify excessive usage per ten second window', () => {
-    const maxCallsPerTenSecondWindow = 8;
+  it('must identify excessive usage per ten minute window', () => {
+    const maxCallsPerTenMinutes = 8;
     isExcessiveUsage = createExcessiveUsageIdentifier({
       maxCalls: Number.MAX_VALUE,
-      maxCallsPerTenSeconds: maxCallsPerTenSecondWindow
+      maxCallsPerTenMinutes,
+      maxCallsPerTenSeconds: Number.MAX_VALUE
     });
 
-    for (let i = 0; i < maxCallsPerTenSecondWindow; i++) {
+    for (let i = 0; i < maxCallsPerTenMinutes; i++) {
       expect(isExcessiveUsage()).to.equal(false);
     }
     expect(isExcessiveUsage()).to.equal(true);
 
     jest.runOnlyPendingTimers();
-    for (let i = 0; i < maxCallsPerTenSecondWindow; i++) {
+    for (let i = 0; i < maxCallsPerTenMinutes; i++) {
+      expect(isExcessiveUsage()).to.equal(false);
+    }
+    expect(isExcessiveUsage()).to.equal(true);
+  });
+
+  it('must identify excessive usage per ten second window', () => {
+    const maxCallsPerTenSeconds = 8;
+    isExcessiveUsage = createExcessiveUsageIdentifier({
+      maxCalls: Number.MAX_VALUE,
+      maxCallsPerTenMinutes: Number.MAX_VALUE,
+      maxCallsPerTenSeconds
+    });
+
+    for (let i = 0; i < maxCallsPerTenSeconds; i++) {
+      expect(isExcessiveUsage()).to.equal(false);
+    }
+    expect(isExcessiveUsage()).to.equal(true);
+
+    jest.runOnlyPendingTimers();
+    for (let i = 0; i < maxCallsPerTenSeconds; i++) {
       expect(isExcessiveUsage()).to.equal(false);
     }
     expect(isExcessiveUsage()).to.equal(true);

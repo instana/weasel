b9be5d499086414a20755288091906e3f3b663e6
Ben Blackmore <bripkens@gmail.com>
2021-03-29 13:59:22 +0200

    Remove unload listener (#73)
    * Remove unload listener
    
    # Why
    
    `unload` makes pages inelligible for bfcache optimizations. This
    might be observed by our customers as part of newer Lighthouse
    audits.
    
    # What
    
    Remove the `unload` event registration and rely on the other
    existing events. This is in line with recent learnings of the
    web-vitals lib.
    
    * Force a try/catch when loading web vitals

---
+++ secureWebVitalsLoader.js
@@ -0,0 +1,18 @@
+/* eslint-env node */
+
+const fs = require('fs');
+
+module.exports = () => ({
+  name: 'secureWebVitalsLoader',
+  load(id) {
+    if (id.endsWith('web-vitals/dist/web-vitals.js')) {
+      console.log('Adding try/catch to the web-vitals module');
+      const content = fs.readFileSync(id, {encoding: 'utf8'});
+      const parts = content.split('export{');
+      if (parts.length !== 2) {
+        throw new Error('web-vitals module must have changed. Cannot auto-wrap it with try/catch.');
+      }
+      return `try {${parts[0]}} catch (e) {}export{${parts[1]}`;
+    }
+  },
+});

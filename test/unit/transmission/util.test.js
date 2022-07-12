47f5f54bc9cc7fc15c78cebb9623cac5b9244ff8
Grace Wang <wangzhih@cn.ibm.com>
2022-07-12 12:25:26 +0800

    reportingBackends take precedence over reportingUrl

---
+++ test/unit/transmission/util.test.js
@@ -0,0 +1,31 @@
+import {isTransmitionRequest} from '../../../lib/transmission/util';
+
+jest.mock('../../../lib/vars');
+
+describe('transmission/util', () => {
+  let varsMock;
+
+  beforeEach(() => {
+    varsMock = require('../../../lib/vars').default;
+    varsMock.reportingUrl = 'http://eum1.example.com';
+    varsMock.reportingBackends = [];
+  });
+
+  it('Check transmission request', () => {
+    expect(isTransmitionRequest('http://eum1.example.com')).toBeTruthy();
+    expect(isTransmitionRequest('http://eum1.example.com/')).toBeTruthy();
+    expect(isTransmitionRequest('http://eum2.example.com')).not.toBeTruthy();
+    expect(isTransmitionRequest('http://eum2.example.com/')).not.toBeTruthy();
+    expect(isTransmitionRequest('http://eum.example.com')).not.toBeTruthy();
+  });
+
+  it('Check transmission request with multiple backends', () => {
+    varsMock.reportingBackends = [{reportingUrl: 'http://eum2.example.com', key: 'key'}];
+    expect(isTransmitionRequest('http://eum1.example.com')).not.toBeTruthy();
+    expect(isTransmitionRequest('http://eum1.example.com/')).not.toBeTruthy();
+    expect(isTransmitionRequest('http://eum2.example.com')).toBeTruthy();
+    expect(isTransmitionRequest('http://eum2.example.com/')).toBeTruthy();
+    expect(isTransmitionRequest('http://eum.example.com')).not.toBeTruthy();
+  });
+
+});

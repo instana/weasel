6406e02b194e3bc67d661cc11224ea1101abe8b0
Grace Wang <wangzhih@cn.ibm.com>
2022-07-12 14:56:59 +0800

    Minor fix

--- test/unit/transmission/util.test.js
+++ test/unit/transmission/util.test.js
@@ -14,17 +14,25 @@ describe('transmission/util', () => {
   it('Check transmission request', () => {
     expect(isTransmitionRequest('http://eum1.example.com')).toBeTruthy();
     expect(isTransmitionRequest('http://eum1.example.com/')).toBeTruthy();
+    expect(isTransmitionRequest('http://eum1.example.com/eum.min.js')).not.toBeTruthy();
     expect(isTransmitionRequest('http://eum2.example.com')).not.toBeTruthy();
     expect(isTransmitionRequest('http://eum2.example.com/')).not.toBeTruthy();
     expect(isTransmitionRequest('http://eum.example.com')).not.toBeTruthy();
   });
 
   it('Check transmission request with multiple backends', () => {
-    varsMock.reportingBackends = [{reportingUrl: 'http://eum2.example.com', key: 'key'}];
+    varsMock.reportingBackends = [
+      { reportingUrl: 'http://eum2.example.com', key: 'key' },
+      { reportingUrl: 'http://eum3.example.com', key: 'key' }
+    ];
     expect(isTransmitionRequest('http://eum1.example.com')).not.toBeTruthy();
     expect(isTransmitionRequest('http://eum1.example.com/')).not.toBeTruthy();
     expect(isTransmitionRequest('http://eum2.example.com')).toBeTruthy();
     expect(isTransmitionRequest('http://eum2.example.com/')).toBeTruthy();
+    expect(isTransmitionRequest('http://eum2.example.com/eum.min.js')).not.toBeTruthy();
+    expect(isTransmitionRequest('http://eum3.example.com')).toBeTruthy();
+    expect(isTransmitionRequest('http://eum3.example.com/')).toBeTruthy();
+    expect(isTransmitionRequest('http://eum3.example.com/eum.min.js')).not.toBeTruthy();
     expect(isTransmitionRequest('http://eum.example.com')).not.toBeTruthy();
   });
 

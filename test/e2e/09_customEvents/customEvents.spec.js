a91bb640744b771808527cdf9c3d33a755cab021
zhouhn <zhouhn@cn.ibm.com>
2023-02-09 17:37:43 +0800

    add test case for backend trace id

--- test/e2e/09_customEvents/customEvents.spec.js
+++ test/e2e/09_customEvents/customEvents.spec.js
@@ -34,6 +34,50 @@ describe('09_customEvents', () => {
             cexpect(beacon.m_age).to.equal('23');
             cexpect(beacon.m_kind).to.equal('experienced');
             cexpect(beacon.m_state).to.equal('broken');
+            cexpect(beacon.bt).to.equal('ab87128a1ff99345');
+          });
+        });
+      });
+    });
+  });
+
+  describe('simpleEventReportingWithoutBackendTraceId', () => {
+    beforeEach(() => {
+      browser.get(getE2ETestBaseUrl('09_customEvents/simpleEventReportingWithoutBackendTraceId'));
+    });
+
+    it('must report custom events', () => {
+      return retry(() => {
+        return getBeacons().then(beacons => {
+          const pageLoadBeacon = expectOneMatching(beacons, beacon => {
+            cexpect(beacon.ty).to.equal('pl');
+          });
+
+          expectOneMatching(beacons, beacon => {
+            cexpect(beacon.ty).to.equal('cus');
+            cexpect(beacon.ts).to.be.a('string');
+            cexpect(beacon.n).to.equal('testWithEmptyBackendTraceId');
+            cexpect(beacon.e).to.match(/Testing 123/);
+            cexpect(beacon.pl).to.equal(pageLoadBeacon.t);
+            cexpect(beacon.bt).to.be.undefined;
+          });
+
+          expectOneMatching(beacons, beacon => {
+            cexpect(beacon.ty).to.equal('cus');
+            cexpect(beacon.ts).to.be.a('string');
+            cexpect(beacon.n).to.equal('testWithNoBackendTraceId');
+            cexpect(beacon.e).to.match(/something wrong/);
+            cexpect(beacon.pl).to.equal(pageLoadBeacon.t);
+            cexpect(beacon.bt).to.be.undefined;
+          });
+
+          expectOneMatching(beacons, beacon => {
+            cexpect(beacon.ty).to.equal('cus');
+            cexpect(beacon.ts).to.be.a('string');
+            cexpect(beacon.n).to.equal('testWithInvalidBackendTraceId');
+            cexpect(beacon.e).to.match(/something wrong/);
+            cexpect(beacon.pl).to.equal(pageLoadBeacon.t);
+            cexpect(beacon.bt).to.be.undefined;
           });
         });
       });

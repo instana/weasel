53f9c2ef5588186008829312a6e6f2bdb2cfea07
Gouri Hariharan <Gouri.Hariharan1@ibm.com>
2024-02-28 10:17:25 +0530

    Added customMetric in Custom Event beacon (#27)
    * Added customMetric
    
    * Update lib/customEvents.js with indent
    
    Co-authored-by: HUAI NAN ZHOU <zhouhn@cn.ibm.com>
    
    * Added e2e test for customMetric in weasel
    
    * e2e test for exponential value cm
    
    ---------
    
    Co-authored-by: Gouri-Hariharan1 <“gouri.hariharan1@ibm.com”>
    Co-authored-by: HUAI NAN ZHOU <zhouhn@cn.ibm.com>

--- test/e2e/09_customEvents/customEvents.spec.js
+++ test/e2e/09_customEvents/customEvents.spec.js
@@ -35,6 +35,7 @@ describe('09_customEvents', () => {
             cexpect(beacon.m_kind).to.equal('experienced');
             cexpect(beacon.m_state).to.equal('broken');
             cexpect(beacon.bt).to.equal('ab87128a1ff99345');
+            cexpect(beacon.cm).to.equal('123.2342');
           });
         });
       });
@@ -78,6 +79,62 @@ describe('09_customEvents', () => {
             cexpect(beacon.e).to.match(/something wrong/);
             cexpect(beacon.pl).to.equal(pageLoadBeacon.t);
             cexpect(beacon.bt).to.be.undefined;
+          });
+        });
+      });
+    });
+  });
+
+  describe('simpleEventReportingWithoutCustomMetric', () => {
+    beforeEach(() => {
+      browser.get(getE2ETestBaseUrl('09_customEvents/simpleEventReportingWithoutCustomMetric'));
+    });
+
+    it('must report custom events', () => {
+      return retry(() => {
+        return getBeacons().then(beacons => {
+          const pageLoadBeacon = expectOneMatching(beacons, beacon => {
+            cexpect(beacon.ty).to.equal('pl');
+          });
+
+          expectOneMatching(beacons, beacon => {
+            cexpect(beacon.ty).to.equal('cus');
+            cexpect(beacon.ts).to.be.a('string');
+            cexpect(beacon.n).to.equal('testWithEmptyCustomMetric');
+            cexpect(beacon.e).to.match(/Testing 123/);
+            cexpect(beacon.pl).to.equal(pageLoadBeacon.t);
+            cexpect(beacon.bt).to.equal('ab87128a1ff99345');
+            cexpect(beacon.cm).to.be.undefined;
+          });
+
+          expectOneMatching(beacons, beacon => {
+            cexpect(beacon.ty).to.equal('cus');
+            cexpect(beacon.ts).to.be.a('string');
+            cexpect(beacon.n).to.equal('testWithNoCustomMetric');
+            cexpect(beacon.e).to.match(/something wrong/);
+            cexpect(beacon.pl).to.equal(pageLoadBeacon.t);
+            cexpect(beacon.bt).to.equal('ab87128a1ff99345');
+            cexpect(beacon.cm).to.be.undefined;
+          });
+
+          expectOneMatching(beacons, beacon => {
+            cexpect(beacon.ty).to.equal('cus');
+            cexpect(beacon.ts).to.be.a('string');
+            cexpect(beacon.n).to.equal('testWithIncorrectCustomMetric');
+            cexpect(beacon.e).to.match(/something wrong/);
+            cexpect(beacon.pl).to.equal(pageLoadBeacon.t);
+            cexpect(beacon.bt).to.equal('ab87128a1ff99345');
+            cexpect(beacon.cm).to.be.undefined;
+          });
+
+          expectOneMatching(beacons, beacon => {
+            cexpect(beacon.ty).to.equal('cus');
+            cexpect(beacon.ts).to.be.a('string');
+            cexpect(beacon.n).to.equal('testWithExponentialCustomMetric(1e5)');
+            cexpect(beacon.e).to.match(/something wrong/);
+            cexpect(beacon.pl).to.equal(pageLoadBeacon.t);
+            cexpect(beacon.bt).to.equal('ab87128a1ff99345');
+            cexpect(beacon.cm).to.equal('100000');
           });
         });
       });

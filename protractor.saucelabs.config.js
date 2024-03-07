873b2c31a3b5711346541e64b676ee874209539a
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-03-07 17:25:01 +0530

    test with browserName: chrome,browserVersion: latest,platformName: Windows 11

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -9,17 +9,18 @@ exports.config = {
   sauceBuild: process.env.GITHUB_RUN_NUMBER,
   // See https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
   multiCapabilities: [
-    newSaucelabsCapability('internet explorer', '11.103', 'Windows 10'),
-    newSaucelabsCapability('MicrosoftEdge', '14.14393', 'Windows 10'),
-    newSaucelabsCapability('safari', '9.0', 'OS X 10.11'),
-    newSaucelabsCapability('safari', '10.1', 'macOS 10.12'),
-    newSaucelabsCapability('safari', '11.0', 'macOS 10.12'),
-    newSaucelabsCapability('safari', '11.1', 'macOS 10.13'),
-    newSaucelabsCapability('firefox', '78.0', 'Windows 7'),
-    newSaucelabsCapability('firefox', '58.0', 'Windows 11'),
-    newSaucelabsCapability('chrome', '67.0', 'Windows 10'),
-    newSaucelabsCapability('chrome', '54.0', 'OS X 10.11'),
-    newSaucelabsCapability('chrome', '65.0', 'OS X 10.11')
+    // newSaucelabsCapability('internet explorer', '11.103', 'Windows 10'),
+    // newSaucelabsCapability('MicrosoftEdge', '14.14393', 'Windows 10'),
+    // newSaucelabsCapability('safari', '9.0', 'OS X 10.11'),
+    // newSaucelabsCapability('safari', '10.1', 'macOS 10.12'),
+    // newSaucelabsCapability('safari', '11.0', 'macOS 10.12'),
+    // newSaucelabsCapability('safari', '11.1', 'macOS 10.13'),
+    // newSaucelabsCapability('firefox', '78.0', 'Windows 7'),
+    // newSaucelabsCapability('firefox', '58.0', 'Windows 11'),
+    // newSaucelabsCapability('chrome', '67.0', 'Windows 10'),
+    // newSaucelabsCapability('chrome', '54.0', 'OS X 10.11'),
+    // newSaucelabsCapability('chrome', '65.0', 'OS X 10.11'),
+    newSaucelabsCapability('chrome', 'latest', 'Windows 11')
   ],
   // Do not allow parallel test execution. Makes the test execution a lot
   // slower, but the setup simpler.

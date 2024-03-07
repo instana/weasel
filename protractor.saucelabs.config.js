54d364cdd617921fd795c97e2eb3c2e9705ba9d8
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-03-07 17:40:55 +0530

    'MicrosoftEdge', '14.14393', 'Windows 10'

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -9,8 +9,9 @@ exports.config = {
   sauceBuild: process.env.GITHUB_RUN_NUMBER,
   // See https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
   multiCapabilities: [
-    newSaucelabsCapability('internet explorer', '11.103', 'Windows 10'),
-    // newSaucelabsCapability('MicrosoftEdge', '14.14393', 'Windows 10'),
+    // newSaucelabsCapability('chrome', 'latest', 'Windows 11')
+    // newSaucelabsCapability('internet explorer', '11.103', 'Windows 10'),
+    newSaucelabsCapability('MicrosoftEdge', '14.14393', 'Windows 10'),
     // newSaucelabsCapability('safari', '9.0', 'OS X 10.11'),
     // newSaucelabsCapability('safari', '10.1', 'macOS 10.12'),
     // newSaucelabsCapability('safari', '11.0', 'macOS 10.12'),
@@ -20,7 +21,6 @@ exports.config = {
     // newSaucelabsCapability('chrome', '67.0', 'Windows 10'),
     // newSaucelabsCapability('chrome', '54.0', 'OS X 10.11'),
     // newSaucelabsCapability('chrome', '65.0', 'OS X 10.11'),
-    newSaucelabsCapability('chrome', 'latest', 'Windows 11')
   ],
   // Do not allow parallel test execution. Makes the test execution a lot
   // slower, but the setup simpler.

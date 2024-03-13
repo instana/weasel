61976766718b1dc43cfaf1b5422cf3a58ae2cb8d
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-03-13 13:12:11 +0530

    chore(TS migration): migrate lib/types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,282 +1,278 @@
 // @flow
-/* eslint-disable no-undef */
 
-export type ShortBoolean = 0 | 1;
+export type ShortBoolean = 0 | 1 | '0' | '1';
 
 export type Meta = {
-  [key: string]: ?string|?number
-}
+  [key: string]: string | number | null | undefined;
+};
 
 export type Event = {
-  initialize: () => mixed,
-  name: string,
-  time: ?number
-}
+  initialize: () => unknown;
+  name: string;
+  time?: number | null;
+};
 
 export type State = {
-  onEnter: () => mixed,
-  getActiveTraceId(): ?string,
-  getActivePhase(): ?string
-}
+  onEnter: () => unknown;
+  getActiveTraceId(): string | null | undefined;
+  getActivePhase(): string | null | undefined;
+};
 
 export type ReportingBackend = {
-  reportingUrl: string,
-  key: ?string
+  reportingUrl: string;
+  key: string | null | undefined;
 };
 
 export interface Beacon {
   // The API key.
-  k: string,
+  k: string;
 
   // The version of the tracking snippet
-  sv: ?string,
+  sv: string | null | undefined;
 
   // The trace ID of this beacon.
-  t: string,
+  t: string;
 
   // start timestamp in millis
-  ts: number,
+  ts: number;
 
   // location, as in the whole URL from window.location.href
-  l: string,
+  l: string;
 
   // name of the page
-  p: ?string,
+  p: string | null | undefined;
 
   // currently active phase of the page (see phases)
-  ph: ?string,
+  ph: string | null | undefined;
 
   // user id
-  ui: ?string,
+  ui: string | null | undefined;
 
   // user name
-  un: ?string,
+  un: string | null | undefined;
 
   // user email address
-  ue: ?string,
+  ue: string | null | undefined;
 
   // letter code for user preferred language, e.g. de, en-US
-  ul: ?string,
+  ul: string | null | undefined;
 
   // Session ID
-  sid: ?string,
+  sid: string | null | undefined;
 
   // window.innerWidth
-  ww: ?number,
+  ww?: number;
 
   // window.innerHeight
-  wh: ?number,
+  wh?: number;
 
   // Effective network connection type
   // https://wicg.github.io/netinfo/#dfn-effective-connection-type
-  ct: ?string,
+  ct: string | null | undefined;
 
   // Meta data
   // m_abc: string,
-  [key: string]: ?number|?string,
+  [key: string]: number | string | null | undefined;
 
   // Agent Version
-  agv: ?string
+  agv?: string;
 }
 
 export interface BeaconWithResourceTiming extends Beacon {
   // The reference timestamp. All timestamps are resolved against this one via:
   // timestampToTransmit = actualTimestamp - referenceTimestamp
-  r: number,
+  r: number;
 
   // JSON-serialized resource timing trie
-  res: string
+  res: string;
 }
 
 export interface PageLoadBeacon extends Beacon, BeaconWithResourceTiming {
-  ty: 'pl',
+  ty: 'pl';
 
   // The reference timestamp. All timestamps are resolved against this one via:
   // timestampToTransmit = actualTimestamp - referenceTimestamp
-  r: number,
+  r: number;
 
   // duration in millis
-  d: number,
+  d: number;
 
   // A backend trace ID when available
-  bt: string,
+  bt: string;
 
   // timing data available?
-  tim: ShortBoolean,
+  tim: ShortBoolean;
 
-  t_unl: number,
-  t_red: number,
-  t_apc: number,
-  t_dns: number,
-  t_tcp: number,
-  t_ssl: number,
-  t_req: number,
-  t_rsp: number,
-  t_dom: number,
-  t_chi: number,
-  t_bac: number,
-  t_fro: number,
-  t_fp: number,
-  t_fcp: number,
-  t_lcp: number,
-  t_pro: number,
-  t_loa: number,
-  t_ttfb: number,
-  t_fid: number,
-  t_cls: number
+  t_unl: number;
+  t_red: number;
+  t_apc: number;
+  t_dns: number;
+  t_tcp: number;
+  t_ssl: number;
+  t_req: number;
+  t_rsp: number;
+  t_dom: number;
+  t_chi: number;
+  t_bac: number;
+  t_fro: number;
+  t_fp: number;
+  t_fcp: number;
+  t_lcp: number;
+  t_pro: number;
+  t_loa: number;
+  t_ttfb: number;
+  t_fid: number;
+  t_cls: number;
 }
-
 
 export interface XhrBeacon extends Beacon {
   // The reference timestamp. All timestamps are resolved against this one via:
   // timestampToTransmit = actualTimestamp - referenceTimestamp
-  r: number,
+  r: number;
 
   // duration in millis
-  d: number,
+  d: number;
 
-  ty: 'xhr',
+  ty: 'xhr';
 
   // the span ID of this beacon
-  s: string,
+  s: string;
 
   // the ID of the page load
-  pl: string,
+  pl: string;
 
   // method
-  m: string,
+  m: string;
 
   // url as specified in the user request. May not contain schema / host
-  u: string,
+  u: string;
 
   // async
-  a: ShortBoolean,
+  a: ShortBoolean;
 
   // status code
-  st: number,
+  st: number;
 
   // error message
-  e: ?string,
+  e: string | null | undefined;
 
   // Whether backend correlation was enabled for this XHR call
-  bc: ShortBoolean,
+  bc: ShortBoolean;
 
   // A backend trace ID when available
-  bt: ?string,
+  bt: string | null | undefined;
 
   // Whether the document was hidden at the time the request was initiated
-  h: ShortBoolean,
+  h: ShortBoolean;
 
   // GraphQL operation type
-  got: ?string,
+  got: string | null | undefined;
   // GraphQL operation name
-  gon: ?string,
+  gon: string | null | undefined;
 
   // Resource timing data
   // https://www.w3.org/TR/resource-timing-2/#processing-model
-  t_red: ?number,
-  t_apc: ?number,
-  t_dns: ?number,
-  t_tcp: ?number,
-  t_ssl: ?number,
-  t_req: ?number,
-  t_rsp: ?number,
+  t_red?: number;
+  t_apc?: number;
+  t_dns?: number;
+  t_tcp?: number;
+  t_ssl?: number;
+  t_req?: number;
+  t_rsp?: number;
   // TODO implement
-  t_ttfb: ?number,
-  s_ty: ?number,
-  s_eb: ?number,
-  s_db: ?number,
-  s_ts: ?number,
+  t_ttfb?: number;
+  s_ty?: number;
+  s_eb?: number;
+  s_db?: number;
+  s_ts?: number;
 
   // HTTP header
-  // h_abc: string,
-  [key: string]: ?number|?string
+  // h_abc: string;
+  [key: string]: number | string | null | undefined;
 }
 
-
 export interface UnhandledErrorBeacon extends Beacon {
-  ty: 'err',
+  ty: 'err';
 
   // span id
-  s: string,
+  s: string;
 
   // the ID of the page load
-  pl: string,
+  pl: string;
 
   // error message
-  e: string,
+  e: string;
 
   // error stack
-  st: string,
+  st: string;
 
   // component stack
-  cs: ?string,
+  cs: string | null | undefined;
 
   // how many times this error was seen since the last transmission
-  c: number
+  c: number;
 }
 
 export interface ReportErrorOpts {
   // for key based access to avoid global wrangling by closure compiler
-  [key: string]: ?string|?Meta,
+  [key: string]: string | null | undefined | Meta;
 
-  componentStack: ?string,
+  componentStack: string | null | undefined;
 
   // Generally useful to have one-off meta data
-  meta: ?Meta
+  meta?: Meta;
 }
 
-
 export interface CustomEventBeacon extends Beacon {
-  ty: 'cus',
+  ty: 'cus';
 
   // event name
-  n: string,
+  n: string;
 
   // duration in millis
-  d: ?number,
+  d?: number;
 
   // A backend trace ID when available
-  bt: ?string,
+  bt: string | null | undefined;
 
   // error message
-  e: ?string,
+  e: string | null | undefined;
   // error stack
-  st: ?string,
+  st: string | null | undefined;
   // component stack
-  cs: ?string,
+  cs: string | null | undefined;
 
   //custom metric
-  cm: ?number
+  cm?: number;
 }
 
 export interface CustomEventOptions {
   // To overwrite the default timestamp of the event. By default the
   // timestamp is `now() - duration`.
-  timestamp: ?number,
+  timestamp?: number;
 
   // In case the event is something that has spaned a time window.
-  duration: ?number,
+  duration?: number;
 
   // To be used for non-HTTP based backend communication, e.g.
   // custom WebSocket based RPC mechanisms.
-  backendTraceId: ?string,
+  backendTraceId: string | null | undefined;
 
   // Used when integrating Weasel with log appenders
-  error: ?ErrorLike,
-  componentStack: ?string,
+  error?: ErrorLike;
+  componentStack: string | null | undefined;
 
   // Generally useful to have one-off meta data
-  meta: ?Meta,
+  meta?: Meta;
 
   // to permit ['foo'] based access that doesn't break
   // when pushed through the Closure compiler
-  [key: string]: any,
+  [key: string]: any;
 
   // Any custom metric that can be passed with custom events Example: 123.2342
-  customMetric: ?number
+  customMetric?: number;
 }
 
 export interface PageChangeBeacon extends Beacon {
-  ty: 'pc',
+  ty: 'pc';
 }

689ce70f32327c07d675a1345566613bb21c4197
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-04-01 17:15:55 +0530

    chore(TS migration): migrate lib/debug.ts

--- lib/debug.ts
+++ lib/debug.ts
@@ -4,31 +4,26 @@
 
 import {noop} from './util';
 
-type Logger = (...any) => void;
+type Logger = (...args: any[]) => void;
 
-// $FlowFixMe The function is never going to be used when it is a bool
-export const log: Logger = DEBUG && createLogger('log');
-// $FlowFixMe The function is never going to be used when it is a bool
-export const info: Logger = DEBUG && createLogger('info');
-// $FlowFixMe The function is never going to be used when it is a bool
-export const warn: Logger = DEBUG && createLogger('warn');
-// $FlowFixMe The function is never going to be used when it is a bool
-export const error: Logger = DEBUG && createLogger('error');
-// $FlowFixMe The function is never going to be used when it is a bool
-export const debug: Logger = DEBUG && createLogger('debug');
+export const log: Logger = DEBUG ? createLogger('log') : noop;
+export const info: Logger = DEBUG ? createLogger('info') : noop;
+export const warn: Logger = DEBUG ? createLogger('warn') : noop;
+export const error: Logger = DEBUG ? createLogger('error') : noop;
+export const debug: Logger = DEBUG ? createLogger('debug') : noop;
 
-function createLogger(method): (...any) => void {
+function createLogger(method: Extract<keyof Console, 'log' | 'info' | 'warn' | 'error' | 'debug'>): Logger {
   if (typeof console === 'undefined' || typeof console.log !== 'function' || typeof console.log.apply !== 'function') {
     return noop;
   }
 
   if (console[method] && typeof console[method].apply === 'function') {
     return function() {
-      console[method].apply(console, arguments);
+      console[method].apply(console, arguments as any);
     };
   }
 
   return function() {
-    console.log.apply(console, arguments);
+    console.log.apply(console, arguments as any);
   };
 }

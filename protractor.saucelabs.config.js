ac32a440ce727f9fc0264b308533ac0ed5cc8d91
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-05-21 12:33:17 +0530

    old

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -9,6 +9,18 @@ exports.config = {
   sauceBuild: process.env.GITHUB_RUN_NUMBER,
   // See https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
   multiCapabilities: [
+    newSaucelabsCapability('internet explorer', '11.103', 'Windows 10'),
+    newSaucelabsCapability('MicrosoftEdge', '14.14393', 'Windows 10'),
+    newSaucelabsCapability('safari', '9.0', 'OS X 10.11'),
+    newSaucelabsCapability('safari', '10.1', 'macOS 10.12'),
+    newSaucelabsCapability('safari', '11.0', 'macOS 10.12'),
+    newSaucelabsCapability('safari', '11.1', 'macOS 10.13'),
+    newSaucelabsCapability('firefox', '78.0', 'Windows 7'),
+    newSaucelabsCapability('firefox', '58.0', 'Windows 11'),
+    newSaucelabsCapability('chrome', '67.0', 'Windows 10'),
+    newSaucelabsCapability('chrome', '54.0', 'OS X 10.11'),
+    newSaucelabsCapability('chrome', '65.0', 'OS X 10.11'),
+
     newSaucelabsCapability('chrome', 'latest', 'Windows 11', 'TEST FOR latest chrome - Windows 11'),
     newSaucelabsCapability('edge', 'latest', 'Windows 11', 'TEST FOR latest edge - Windows 11'),
     newSaucelabsCapability('firefox', 'latest', 'Windows 11', 'TEST FOR latest firefox - Windows 11'),

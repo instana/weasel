f3b826e40421123aad337ea542eadae4952129d7
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-06 14:03:33 +0530

    test

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -10,19 +10,20 @@ exports.config = {
   // See https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
   multiCapabilities: [
     // Old browser+OS platforms for non-web-vitals tests
-    newSaucelabsCapability('internet explorer', '11.103', 'Windows 10'),
-    newSaucelabsCapability('MicrosoftEdge', '14.14393', 'Windows 10'),
-    newSaucelabsCapability('safari', '9.0', 'OS X 10.11'),
-    newSaucelabsCapability('safari', '10.1', 'macOS 10.12'),
-    newSaucelabsCapability('safari', '11.0', 'macOS 10.12'),
-    newSaucelabsCapability('safari', '11.1', 'macOS 10.13'),
-    newSaucelabsCapability('firefox', '78.0', 'Windows 7'),
-    newSaucelabsCapability('firefox', '58.0', 'Windows 11'),
-    newSaucelabsCapability('chrome', '67.0', 'Windows 10'),
-    newSaucelabsCapability('chrome', '54.0', 'OS X 10.11'),
+    // newSaucelabsCapability('internet explorer', '11.103', 'Windows 10'),
+    // newSaucelabsCapability('MicrosoftEdge', '14.14393', 'Windows 10'),
+    // newSaucelabsCapability('safari', '9.0', 'OS X 10.11'),
+    // newSaucelabsCapability('safari', '10.1', 'macOS 10.12'),
+    // newSaucelabsCapability('safari', '11.0', 'macOS 10.12'),
+    // newSaucelabsCapability('safari', '11.1', 'macOS 10.13'),
+    // newSaucelabsCapability('firefox', '78.0', 'Windows 7'),
+    // newSaucelabsCapability('firefox', '58.0', 'Windows 11'),
+    // newSaucelabsCapability('chrome', '67.0', 'Windows 10'),
+    // newSaucelabsCapability('chrome', '54.0', 'OS X 10.11'),
     newSaucelabsCapability('chrome', '65.0', 'OS X 10.11'),
 
     // Specific supported browser+OS for web-vitals tests
+    newSaucelabsCapability('chrome', '124.0', 'macOS 11.00', true),
     newSaucelabsCapability('chrome', '80.0', 'Windows 10', true),
     newSaucelabsCapability('chrome', '83.0', 'OS X 10.11', true),
     newSaucelabsCapability('MicrosoftEdge', '80.0', 'Windows 10', true),

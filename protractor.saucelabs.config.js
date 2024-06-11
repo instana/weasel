b255b9baa8adf3a53ed4be5fe8c5b7b9dbde6411
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-11 10:40:56 +0530

    sauceLab config updated

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -1,48 +1,46 @@
 /* eslint-env node */
 
 exports.config = {
-  specs: ['test/e2e/**/*.spec.js'],
-  // TODO: disable webvital tests for saucelab for now, since browsers in saucelab seems never return webvital metrics
-  // exclude: ['test/e2e/12_webvitalsAsCustomEvent/*.spec.js'],
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
   sauceBuild: process.env.GITHUB_RUN_NUMBER,
   // See https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
   multiCapabilities: [
-    // newSaucelabsCapability('internet explorer', '11.103', 'Windows 10'),
-    // newSaucelabsCapability('MicrosoftEdge', '14.14393', 'Windows 10'),
-    // newSaucelabsCapability('safari', '9.0', 'OS X 10.11'),
-    // newSaucelabsCapability('safari', '10.1', 'macOS 10.12'),
-    // newSaucelabsCapability('safari', '11.0', 'macOS 10.12'),
-    // newSaucelabsCapability('safari', '11.1', 'macOS 10.13'),
-    // newSaucelabsCapability('firefox', '78.0', 'Windows 7'),
-    // newSaucelabsCapability('firefox', '58.0', 'Windows 11'),
-    // newSaucelabsCapability('chrome', '67.0', 'Windows 10'),
-    // newSaucelabsCapability('chrome', '54.0', 'OS X 10.11'),
-    // newSaucelabsCapability('chrome', '65.0', 'OS X 10.11'),
+    // Old browser+OS platforms for non-web-vitals tests
+    newSaucelabsCapability('internet explorer', '11.103', 'Windows 10'),
+    newSaucelabsCapability('MicrosoftEdge', '14.14393', 'Windows 10'),
+    newSaucelabsCapability('safari', '9.0', 'OS X 10.11'),
+    newSaucelabsCapability('safari', '10.1', 'macOS 10.12'),
+    newSaucelabsCapability('safari', '11.0', 'macOS 10.12'),
+    newSaucelabsCapability('safari', '11.1', 'macOS 10.13'),
+    newSaucelabsCapability('firefox', '78.0', 'Windows 7'),
+    newSaucelabsCapability('firefox', '58.0', 'Windows 11'),
+    newSaucelabsCapability('chrome', '67.0', 'Windows 10'),
+    newSaucelabsCapability('chrome', '54.0', 'OS X 10.11'),
+    newSaucelabsCapability('chrome', '65.0', 'OS X 10.11'),
 
-    newSaucelabsCapability('chrome', '122', 'Windows 11'),
-    newSaucelabsCapability('chrome', '125', 'Windows 11'),
-    newSaucelabsCapability('chrome', '125', 'Windows 10'),
-    newSaucelabsCapability('chrome', '109', 'Windows 8.1'),
-    newSaucelabsCapability('chrome', '109', 'Windows 8'),
-    newSaucelabsCapability('chrome', '107', 'Windows 7'),
-    newSaucelabsCapability('chrome', '125', 'macOS 13'),
-    newSaucelabsCapability('chrome', '125', 'macOS 12'),
-    newSaucelabsCapability('chrome', '125', 'macOS 11'),
-    newSaucelabsCapability('chrome', '125', 'macOS 10.15'),
-    newSaucelabsCapability('chrome', '116', 'macOS 10.14'),
-    newSaucelabsCapability('chrome', '107', 'macOS 10.13'),
-    newSaucelabsCapability('chrome', '103', 'macOS 10.12'),
-    newSaucelabsCapability('chrome', '103', 'OS X 10.11'),
-
-    newSaucelabsCapability('chrome', 'latest', 'Windows 7'),
-    newSaucelabsCapability('chrome', 'latest', 'Windows 10'),
-    newSaucelabsCapability('chrome', 'latest', 'Windows 11'),
-    newSaucelabsCapability('chrome', 'latest', 'macOS 10.12'),
-    newSaucelabsCapability('chrome', 'latest', 'macOS 10.13'),
-    newSaucelabsCapability('chrome', 'latest', 'macOS 11.0'),
-    newSaucelabsCapability('chrome', 'latest', 'OS X 10.11')
+    // Specific supported browser+OS for web-vitals tests
+    newSaucelabsCapability('chrome', '107', 'Windows 7', true),
+    newSaucelabsCapability('chrome', '109', 'Windows 8', true),
+    newSaucelabsCapability('chrome', '109', 'Windows 8.1', true),
+    newSaucelabsCapability('chrome', '125', 'Windows 10', true),
+    newSaucelabsCapability('chrome', '122', 'Windows 11', true),
+    newSaucelabsCapability('chrome', '125', 'Windows 11', true),
+    newSaucelabsCapability('chrome', '125', 'macOS 11', true),
+    newSaucelabsCapability('chrome', '125', 'macOS 12', true),
+    newSaucelabsCapability('chrome', '125', 'macOS 13', true),
+    newSaucelabsCapability('chrome', '103', 'macOS 10.12', true),
+    newSaucelabsCapability('chrome', '107', 'macOS 10.13', true),
+    newSaucelabsCapability('chrome', '116', 'macOS 10.14', true),
+    newSaucelabsCapability('chrome', '125', 'macOS 10.15', true),
+    newSaucelabsCapability('chrome', '103', 'OS X 10.11', true),
+    newSaucelabsCapability('chrome', 'latest', 'Windows 7', true),
+    newSaucelabsCapability('chrome', 'latest', 'Windows 10', true),
+    newSaucelabsCapability('chrome', 'latest', 'Windows 11', true),
+    newSaucelabsCapability('chrome', 'latest', 'macOS 10.12', true),
+    newSaucelabsCapability('chrome', 'latest', 'macOS 10.13', true),
+    newSaucelabsCapability('chrome', 'latest', 'macOS 11.0', true),
+    newSaucelabsCapability('chrome', 'latest', 'OS X 10.11', true)
   ],
   // Do not allow parallel test execution. Makes the test execution a lot
   // slower, but the setup simpler.
@@ -52,13 +50,15 @@ exports.config = {
   }
 };
 
-function newSaucelabsCapability(browserName, version, platform) {
+function newSaucelabsCapability(browserName, version, platform, isWebVitalsTest = false) {
   return {
     browserName,
     version,
     platform,
-    name: 'weasel e2e',
+    name: isWebVitalsTest ? 'weasel e2e - web vitals' : 'weasel e2e',
     'tunnel-identifier': 'github-action-tunnel',
-    build: process.env.GITHUB_RUN_NUMBER
+    build: process.env.GITHUB_RUN_NUMBER,
+    specs: isWebVitalsTest ? ['test/e2e/12_webvitalsAsCustomEvent/*.spec.js'] : ['test/e2e/**/*.spec.js'],
+    exclude: isWebVitalsTest ? [] : ['test/e2e/12_webvitalsAsCustomEvent/*.spec.js']
   };
 }

8df2f762582ea911b1b99346afa9a7e6869bda56
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-12 14:38:43 +0530

    internet explorer, 11.103, Windows 10 - in FID

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -33,7 +33,7 @@ function newSaucelabsCapability(browserName, version, platform) {
     browserName,
     version,
     platform,
-    name: 'internet explorer, 11.103, Windows 10 - in LCP',
+    name: 'internet explorer, 11.103, Windows 10 - in FID',
     'tunnel-identifier': 'github-action-tunnel',
     build: process.env.GITHUB_RUN_NUMBER
   };

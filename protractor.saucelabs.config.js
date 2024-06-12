e9faced4fd26aba791e237437fdadc2a38b33488
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-12 17:00:36 +0530

    safari, 11.0, macOS 10.12 - in FID

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -33,7 +33,7 @@ function newSaucelabsCapability(browserName, version, platform) {
     browserName,
     version,
     platform,
-    name: 'safari, 11.0, macOS 10.12 - in LCP',
+    name: 'safari, 11.0, macOS 10.12 - in FID',
     'tunnel-identifier': 'github-action-tunnel',
     build: process.env.GITHUB_RUN_NUMBER
   };

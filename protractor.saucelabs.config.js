d3bf599f1b29ef9ad066921f390a567541e75cd5
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-12 16:54:31 +0530

    safari, 11.0, macOS 10.12 - in LCP

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -11,8 +11,8 @@ exports.config = {
     // newSaucelabsCapability('internet explorer', '11.103', 'Windows 10'),
     // newSaucelabsCapability('MicrosoftEdge', '14.14393', 'Windows 10'),
     // newSaucelabsCapability('safari', '9.0', 'OS X 10.11'),
-    newSaucelabsCapability('safari', '10.1', 'macOS 10.12'),
-    // newSaucelabsCapability('safari', '11.0', 'macOS 10.12'),
+    // newSaucelabsCapability('safari', '10.1', 'macOS 10.12'),
+    newSaucelabsCapability('safari', '11.0', 'macOS 10.12'),
     // newSaucelabsCapability('safari', '11.1', 'macOS 10.13'),
     // newSaucelabsCapability('firefox', '78.0', 'Windows 7'),
     // newSaucelabsCapability('firefox', '58.0', 'Windows 11'),
@@ -33,7 +33,7 @@ function newSaucelabsCapability(browserName, version, platform) {
     browserName,
     version,
     platform,
-    name: 'safari, 10.1, macOS 10.12 - in INP',
+    name: 'safari, 11.0, macOS 10.12 - in LCP',
     'tunnel-identifier': 'github-action-tunnel',
     build: process.env.GITHUB_RUN_NUMBER
   };

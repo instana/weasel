233f45917ede511b921ea4a1b1d72b5ed14dbcf0
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-12 11:45:36 +0530

    TEST-

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -1,12 +1,12 @@
-/* eslint-env node */
-
 exports.config = {
+  specs: ['test/e2e/**/*.spec.js'],
+  // TODO: disable webvital tests for saucelab for now, since browsers in saucelab seems never return webvital metrics
+  // exclude: ['test/e2e/12_webvitalsAsCustomEvent/*.spec.js'],
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
   sauceBuild: process.env.GITHUB_RUN_NUMBER,
   // See https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
   multiCapabilities: [
-    // Old browser+OS platforms for non-web-vitals tests
     newSaucelabsCapability('internet explorer', '11.103', 'Windows 10'),
     newSaucelabsCapability('MicrosoftEdge', '14.14393', 'Windows 10'),
     newSaucelabsCapability('safari', '9.0', 'OS X 10.11'),
@@ -17,12 +17,7 @@ exports.config = {
     newSaucelabsCapability('firefox', '58.0', 'Windows 11'),
     newSaucelabsCapability('chrome', '67.0', 'Windows 10'),
     newSaucelabsCapability('chrome', '54.0', 'OS X 10.11'),
-    newSaucelabsCapability('chrome', '65.0', 'OS X 10.11'),
-
-    // Specific supported browser+OS for web-vitals tests.
-    newSaucelabsCapability('chrome', '107', 'Windows 7', true),
-    newSaucelabsCapability('chrome', '125', 'macOS 11', true),
-    newSaucelabsCapability('chrome', '103', 'OS X 10.11', true)
+    newSaucelabsCapability('chrome', '65.0', 'OS X 10.11')
   ],
   // Do not allow parallel test execution. Makes the test execution a lot
   // slower, but the setup simpler.
@@ -32,15 +27,13 @@ exports.config = {
   }
 };
 
-function newSaucelabsCapability(browserName, version, platform, isWebVitalsTest = false) {
+function newSaucelabsCapability(browserName, version, platform) {
   return {
     browserName,
     version,
     platform,
-    name: isWebVitalsTest ? 'weasel e2e - web vitals' : 'weasel e2e',
+    name: 'weasel e2e',
     'tunnel-identifier': 'github-action-tunnel',
-    build: process.env.GITHUB_RUN_NUMBER,
-    specs: isWebVitalsTest ? ['test/e2e/12_webvitalsAsCustomEvent/*.spec.js'] : ['test/e2e/**/*.spec.js'],
-    exclude: isWebVitalsTest ? [] : ['test/e2e/12_webvitalsAsCustomEvent/*.spec.js']
+    build: process.env.GITHUB_RUN_NUMBER
   };
 }

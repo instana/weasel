0575d03fd268a45659faa2df75ea114703ae6a37
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-12 15:23:27 +0530

    MicrosoftEdge, 14.14393, Windows 10 - in CLS

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -33,7 +33,7 @@ function newSaucelabsCapability(browserName, version, platform) {
     browserName,
     version,
     platform,
-    name: 'MicrosoftEdge, 14.14393, Windows 10 - in FID',
+    name: 'MicrosoftEdge, 14.14393, Windows 10 - in CLS',
     'tunnel-identifier': 'github-action-tunnel',
     build: process.env.GITHUB_RUN_NUMBER
   };

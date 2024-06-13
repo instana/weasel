c3a8b98093c81dbcffc82db440ca8d87dd943a6e
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-13 12:06:24 +0530

    chrome, 65.0, OS X 10.11 - in FID

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -33,7 +33,7 @@ function newSaucelabsCapability(browserName, version, platform) {
     browserName,
     version,
     platform,
-    name: 'chrome, 65.0, OS X 10.11 - in LCP',
+    name: 'chrome, 65.0, OS X 10.11 - in FID',
     'tunnel-identifier': 'github-action-tunnel',
     build: process.env.GITHUB_RUN_NUMBER
   };

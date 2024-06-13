46c522fc56cf06077902e39f209d6faa7852404b
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-13 14:37:29 +0530

    firefox, 126, macOS 11.00 - INP

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -37,7 +37,7 @@ function newSaucelabsCapability(browserName, version, platform) {
     browserName,
     version,
     platform,
-    name: 'firefox, 126, macOS 11.00 - CLS',
+    name: 'firefox, 126, macOS 11.00 - INP',
     'tunnel-identifier': 'github-action-tunnel',
     build: process.env.GITHUB_RUN_NUMBER
   };

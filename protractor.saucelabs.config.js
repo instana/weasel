539fd8e7c1579398726cfa14bf6886670f1c13f9
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-13 11:51:44 +0530

    chrome, 65.0, OS X 10.11 - in LCP

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -17,8 +17,8 @@ exports.config = {
     // newSaucelabsCapability('firefox', '78.0', 'Windows 7'),
     // newSaucelabsCapability('firefox', '58.0', 'Windows 11'),
     // newSaucelabsCapability('chrome', '67.0', 'Windows 10'),
-    newSaucelabsCapability('chrome', '54.0', 'OS X 10.11'),
-    // newSaucelabsCapability('chrome', '65.0', 'OS X 10.11')
+    // newSaucelabsCapability('chrome', '54.0', 'OS X 10.11'),
+    newSaucelabsCapability('chrome', '65.0', 'OS X 10.11')
   ],
   // Do not allow parallel test execution. Makes the test execution a lot
   // slower, but the setup simpler.
@@ -33,7 +33,7 @@ function newSaucelabsCapability(browserName, version, platform) {
     browserName,
     version,
     platform,
-    name: 'chrome, 54.0, OS X 10.11 - in INP',
+    name: 'chrome, 65.0, OS X 10.11 - in LCP',
     'tunnel-identifier': 'github-action-tunnel',
     build: process.env.GITHUB_RUN_NUMBER
   };

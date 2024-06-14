3d396ec74846190b00bc3a5120596097cf08b4bd
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-14 13:36:27 +0530

    LCP chrome - 78, macOS 10.12

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -26,19 +26,20 @@ exports.config = {
 
 // LCP
 // LCP - MicrosoftEdge - PASSED
-    newSaucelabsCapability('MicrosoftEdge', '80', 'macOS 10.12'),
-    newSaucelabsCapability('MicrosoftEdge', '80', 'Windows 10'),
-    newSaucelabsCapability('MicrosoftEdge', '80', 'OS X 10.10'),
+    // newSaucelabsCapability('MicrosoftEdge', '80', 'macOS 10.12'),
+    // newSaucelabsCapability('MicrosoftEdge', '80', 'Windows 10'),
+    // newSaucelabsCapability('MicrosoftEdge', '80', 'OS X 10.10'),
 
   // LCP - Chrome - PASSED
-    newSaucelabsCapability('chrome', '78', 'macOS 10.15'), // passed - 315
-    newSaucelabsCapability('chrome', '78', 'Windows 7'), // passed - 316
-    newSaucelabsCapability('chrome', '78', 'OS X 10.10'), // passed 317
+    newSaucelabsCapability('chrome', '78', 'macOS 10.12'),
+    // newSaucelabsCapability('chrome', '78', 'macOS 10.15'), // passed - 315
+    // newSaucelabsCapability('chrome', '78', 'Windows 7'), // passed - 316
+    // newSaucelabsCapability('chrome', '78', 'OS X 10.10'), // passed 317
 
 // Testing
   // LCP - firefox - osx with firefox 122 not supporting from sauceLab config
-    newSaucelabsCapability('firefox', '124', 'macOS 10.15'),// passed - 321
-    newSaucelabsCapability('firefox', '122', 'Windows 10'), // passed - 320
+    // newSaucelabsCapability('firefox', '124', 'macOS 10.15'),// passed - 321
+    // newSaucelabsCapability('firefox', '122', 'Windows 10'), // passed - 320
 
 
   ],
@@ -55,7 +56,7 @@ function newSaucelabsCapability(browserName, version, platform) {
     browserName,
     version,
     platform,
-    name: 'LCP ALL PASS CASES - RETEST',
+    name: 'LCP chrome - 78, macOS 10.12',
     'tunnel-identifier': 'github-action-tunnel',
     build: process.env.GITHUB_RUN_NUMBER
   };

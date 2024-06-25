e59290d28a0db97531696ddb2c553d79c81d1e1a
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-25 15:49:21 +0530

    TTFB

--- test/e2e/12_webvitalsAsCustomEvent/webvitalsAsCustomEvent.spec.js
+++ test/e2e/12_webvitalsAsCustomEvent/webvitalsAsCustomEvent.spec.js
@@ -86,12 +86,11 @@ describe('12_webvitalsAsCustomEvent', () => {
 
           if (capabilities.metricName === 'TTFB') {
             expectOneMatching(beacons, beacon => {
-              cexpect(beacon.ty).to.equal('cus');
+              cexpect(beacon.ty).to.equal('pl');
               cexpect(beacon.ts).to.be.a('string');
-              cexpect(beacon.n).to.equal('instana-webvitals-TTFB');
+              cexpect(beacon.t_ttfb).to.be.a('string');
               cexpect(beacon.l).to.be.a('string');
               cexpect(beacon.pl).to.equal(pageLoadBeacon.t);
-              cexpect(beacon.m_id).to.match(/^v\d+(-\d+)+$/);
             });
           }
         });

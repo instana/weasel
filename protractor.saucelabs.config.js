ca4f04275ef4f21983089af2fa0292603b29251c
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-25 23:06:40 +0530

    FCP -  chrome 86, OS X 10.10

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -111,7 +111,7 @@ exports.config = {
     //   // FCP - Chrome - TEST
     //     newSaucelabsCapability('chrome', '60', 'macOS 10.12', 'FCP'), // passed - 392
     //     newSaucelabsCapability('chrome', '60', 'Windows 7', 'FCP'), // TEST - 393
-        newSaucelabsCapability('chrome', '85', 'OS X 10.10', 'FCP'), // TEST - 60-391, 65-394, 80 - 395, 87 - 396 if all fail - remove this case
+        newSaucelabsCapability('chrome', '86', 'OS X 10.10', 'FCP'), // TEST - 397
 
 
     //   // FCP firefox - TEST
@@ -138,7 +138,7 @@ function newSaucelabsCapability(browserName, version, platform, metricName = '')
     version,
     platform,
     metricName,
-    name: isWebVitalsTest ? `${metricName} chrome 85, OS X 10.10` : 'weasel e2e',
+    name: isWebVitalsTest ? `${metricName} chrome 86, OS X 10.10` : 'weasel e2e',
     'tunnel-identifier': 'github-action-tunnel',
     build: process.env.GITHUB_RUN_NUMBER,
     specs: ['test/e2e/12_webvitalsAsCustomEvent/*.spec.js'],

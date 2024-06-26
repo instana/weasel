b2dea2ea69d8f1f3a0cd83fae0bf1485c40a79f5
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-26 13:27:58 +0530

    INP edge 97

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -8,8 +8,8 @@ exports.config = {
   multiCapabilities: [
     // newSaucelabsCapability('MicrosoftEdge', '97', 'macOS 10.12', 'LCP'),// pass in 80
     // newSaucelabsCapability('MicrosoftEdge', '97', 'macOS 10.12', 'FID'),// pass in 80
-    newSaucelabsCapability('MicrosoftEdge', '97', 'macOS 10.12', 'CLS'),// pass in 80
-    // newSaucelabsCapability('MicrosoftEdge', '97', 'macOS 10.12', 'INP'),// pass in 97
+    // newSaucelabsCapability('MicrosoftEdge', '97', 'macOS 10.12', 'CLS'),// pass in 80
+    newSaucelabsCapability('MicrosoftEdge', '97', 'macOS 10.12', 'INP'),// pass in 97
     // newSaucelabsCapability('MicrosoftEdge', '97', 'macOS 10.12', 'TTFB'),//pass in 79
     // newSaucelabsCapability('MicrosoftEdge', '97', 'macOS 10.12', 'FCP'),//pass in 79
   ],

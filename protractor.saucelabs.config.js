a8031af31c2bf6a7979000db7bb56b5f21b344a0
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-26 17:37:01 +0530

    code optimise -wv test

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -1,17 +1,48 @@
 /* eslint-env node */
-// BUILD - 428
+// BUILD - ???
 exports.config = {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
   sauceBuild: process.env.GITHUB_RUN_NUMBER,
   // See https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
   multiCapabilities: [
-    // newSaucelabsCapability('MicrosoftEdge', '97', 'macOS 10.12', 'LCP'),// pass in 80
-    // newSaucelabsCapability('MicrosoftEdge', '97', 'macOS 10.12', 'FID'),// pass in 80
-    // newSaucelabsCapability('MicrosoftEdge', '97', 'macOS 10.12', 'CLS'),// pass in 80
-    // newSaucelabsCapability('MicrosoftEdge', '97', 'macOS 10.12', 'INP'),// pass in 97
-    // newSaucelabsCapability('MicrosoftEdge', '97', 'macOS 10.12', 'TTFB'),//pass in 79
-    newSaucelabsCapability('MicrosoftEdge', '97', 'macOS 10.12', 'FCP'),//pass in 79
+    // General Capabilities
+    // ...generateCapabilities('internet explorer', '11.103', 'Windows 10'),
+    // ...generateCapabilities('MicrosoftEdge', '14.14393', 'Windows 10'),
+    // ...generateCapabilities('safari', '9.0', 'OS X 10.11'),
+    // ...generateCapabilities('safari', '10.1', 'macOS 10.12'),
+    // ...generateCapabilities('safari', '11.0', 'macOS 10.12'),
+    // ...generateCapabilities('safari', '11.1', 'macOS 10.13'),
+    // ...generateCapabilities('firefox', '78.0', 'Windows 7'),
+    // ...generateCapabilities('firefox', '58.0', 'Windows 11'),
+    // ...generateCapabilities('chrome', '67.0', 'Windows 10'),
+    // ...generateCapabilities('chrome', '54.0', 'OS X 10.11'),
+    // ...generateCapabilities('chrome', '65.0', 'OS X 10.11'),
+    // ...generateCapabilities('chrome', '107', 'Windows 7'),
+    // ...generateCapabilities('chrome', '125', 'macOS 11'),
+    // ...generateCapabilities('chrome', '103', 'OS X 10.11'),
+    // ...generateCapabilities('firefox', '126', 'macOS 11.00'),
+    // Specific Metric Capabilities
+    ...generateMetricsCapabilities('MicrosoftEdge', '80', ['macOS 10.12', 'Windows 10', 'OS X 10.10'], ['INP', 'TTFB', 'FCP']), // done1
+    ...generateMetricsCapabilities('chrome', '78', ['macOS 10.12', 'OS X 10.10'], ['FID', 'INP', 'TTFB', 'FCP']), // done2
+    ...generateCapabilities('chrome', '78', 'Windows 10', { excludes: ['LCP', 'FID', 'INP', 'TTFB', 'FCP'] }), // done 2-1
+    ...generateCapabilities('chrome', '78', 'Windows 7', { excludes: ['CLS', 'FID', 'INP', 'TTFB', 'FCP'] }), // done 2-2
+    ...generateMetricsCapabilities('firefox', '122', ['macOS 10.15', 'Windows 10'], ['FID', 'CLS', 'INP', 'TTFB', 'FCP']), // done3
+    ...generateMetricsCapabilities('chrome', '77', ['macOS 10.12', 'Windows 7', 'OS X 10.10'], ['LCP', 'CLS', 'INP', 'TTFB', 'FCP']), // done4
+    ...generateMetricsCapabilities('firefox', '90', ['macOS 10.12', 'Windows 7'], ['LCP', 'CLS', 'INP', 'TTFB', 'FCP']), // done5
+    ...generateCapabilities('chrome', '78', 'Windows 10', { excludes: ['LCP', 'FID', 'INP', 'TTFB', 'FCP'] }),// done7
+    ...generateMetricsCapabilities('MicrosoftEdge', '97', ['macOS 10.12', 'Windows 10'], ['LCP', 'FID', 'CLS', 'TTFB', 'FCP']),// done8
+    ...generateMetricsCapabilities('chrome', '97', ['macOS 10.12', 'Windows 7', 'OS X 10.11'], ['LCP', 'FID', 'CLS', 'TTFB', 'FCP']),// done9
+    ...generateMetricsCapabilities('MicrosoftEdge', '79', ['macOS 10.12', 'OS X 10.10'], ['LCP', 'FID', 'CLS', 'INP']),// done10
+    ...generateCapabilities('MicrosoftEdge', '13', 'Windows 10', { excludes: ['LCP', 'FID', 'CLS', 'INP', 'FCP'] }), // done10-1
+    ...generateCapabilities('MicrosoftEdge', '92', 'Windows 10', { excludes: ['LCP', 'FID', 'CLS', 'INP', 'TTFB'] }), // done10-2
+    ...generateMetricsCapabilities('chrome', '44', ['macOS 10.12', 'OS X 10.10'], ['LCP', 'FID', 'CLS', 'INP', 'FCP']),// done11
+    ...generateCapabilities('chrome', '67', 'Windows 7', { excludes: ['LCP', 'FID', 'INP', 'TTFB'] }), // done11-1
+    ...generateMetricsCapabilities('firefox', '35', ['OS X 10.10', 'macOS 10.12', 'Windows 7'], ['LCP', 'FID', 'CLS', 'INP', 'FCP']),// done12
+    ...generateCapabilities('safari', '11', 'macOS 10.12', { excludes: ['LCP', 'FID', 'CLS', 'INP', 'FCP'] }),// done13
+    ...generateCapabilities('firefox', '84', 'macOS 10.12', { excludes: ['LCP', 'FID', 'CLS', 'INP', 'TTFB'] }), // done15
+    ...generateCapabilities('firefox', '85', 'Windows 7', { excludes: ['LCP', 'FID', 'CLS', 'INP', 'TTFB'] }), // done16
+    ...generateCapabilities('safari', '15', 'macOS 12', { excludes: ['LCP', 'FID', 'CLS', 'INP', 'TTFB'] }),// done17
   ],
   // Do not allow parallel test execution. Makes the test execution a lot
   // slower, but the setup simpler.
@@ -21,6 +52,25 @@ exports.config = {
   }
 };
 
+const metrics = ['LCP', 'FID', 'CLS', 'INP', 'TTFB', 'FCP'];
+
+function generateCapabilities(browserName, version, platform, options = {}) {
+  const excludes = options.excludes || [];
+  const includes = metrics.filter(metric => !excludes.includes(metric));
+
+  if (includes.length === 0) {
+    // If no metrics are included, return the default capability for e2e tests
+    return [newSaucelabsCapability(browserName, version, platform)];
+  }
+
+  // Create capabilities for each included metric
+  return includes.map(metric => newSaucelabsCapability(browserName, version, platform, metric));
+}
+
+function generateMetricsCapabilities(browserName, version, platforms, excludes) {
+  return platforms.flatMap(platform => generateCapabilities(browserName, version, platform, { excludes }));
+}
+
 function newSaucelabsCapability(browserName, version, platform, metricName = '') {
   const isWebVitalsTest = metricName !== '';
   return {
@@ -28,7 +78,7 @@ function newSaucelabsCapability(browserName, version, platform, metricName = '')
     version,
     platform,
     metricName,
-    name: isWebVitalsTest ? `weasel e2e - ${metricName} 'MicrosoftEdge', '97', 'macOS 10.12'` : 'weasel e2e',
+    name: isWebVitalsTest ? `weasel e2e - ${metricName} web vitals` : 'weasel e2e',
     'tunnel-identifier': 'github-action-tunnel',
     build: process.env.GITHUB_RUN_NUMBER,
     specs: isWebVitalsTest ? ['test/e2e/12_webvitalsAsCustomEvent/*.spec.js'] : ['test/e2e/**/*.spec.js'],

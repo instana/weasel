02fc1ba533beb9dbf6b1ee7f232a1f062dd26b85
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-26 17:59:24 +0530

    TEST ALL

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -1,5 +1,8 @@
 /* eslint-env node */
-// BUILD - ???
+const metrics = ['LCP', 'FID', 'CLS', 'INP', 'TTFB', 'FCP'];
+const excludeTTFB = ['LCP', 'FID', 'CLS', 'INP', 'FCP'];
+const excludeFCP = ['LCP', 'FID', 'CLS', 'INP', 'TTFB'];
+
 exports.config = {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
@@ -7,21 +10,21 @@ exports.config = {
   // See https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
   multiCapabilities: [
     // General Capabilities
-    // ...generateCapabilities('internet explorer', '11.103', 'Windows 10'),
-    // ...generateCapabilities('MicrosoftEdge', '14.14393', 'Windows 10'),
-    // ...generateCapabilities('safari', '9.0', 'OS X 10.11'),
-    // ...generateCapabilities('safari', '10.1', 'macOS 10.12'),
-    // ...generateCapabilities('safari', '11.0', 'macOS 10.12'),
-    // ...generateCapabilities('safari', '11.1', 'macOS 10.13'),
-    // ...generateCapabilities('firefox', '78.0', 'Windows 7'),
-    // ...generateCapabilities('firefox', '58.0', 'Windows 11'),
-    // ...generateCapabilities('chrome', '67.0', 'Windows 10'),
-    // ...generateCapabilities('chrome', '54.0', 'OS X 10.11'),
-    // ...generateCapabilities('chrome', '65.0', 'OS X 10.11'),
-    // ...generateCapabilities('chrome', '107', 'Windows 7'),
-    // ...generateCapabilities('chrome', '125', 'macOS 11'),
-    // ...generateCapabilities('chrome', '103', 'OS X 10.11'),
-    // ...generateCapabilities('firefox', '126', 'macOS 11.00'),
+    ...generateCapabilities('internet explorer', '11.103', 'Windows 10'),
+    ...generateCapabilities('MicrosoftEdge', '14.14393', 'Windows 10'),
+    ...generateCapabilities('safari', '9.0', 'OS X 10.11'),
+    ...generateCapabilities('safari', '10.1', 'macOS 10.12'),
+    ...generateCapabilities('safari', '11.0', 'macOS 10.12'),
+    ...generateCapabilities('safari', '11.1', 'macOS 10.13'),
+    ...generateCapabilities('firefox', '78.0', 'Windows 7'),
+    ...generateCapabilities('firefox', '58.0', 'Windows 11'),
+    ...generateCapabilities('chrome', '67.0', 'Windows 10'),
+    ...generateCapabilities('chrome', '54.0', 'OS X 10.11'),
+    ...generateCapabilities('chrome', '65.0', 'OS X 10.11'),
+    ...generateCapabilities('chrome', '107', 'Windows 7'),
+    ...generateCapabilities('chrome', '125', 'macOS 11'),
+    ...generateCapabilities('chrome', '103', 'OS X 10.11'),
+    ...generateCapabilities('firefox', '126', 'macOS 11.00'),
     // Specific Metric Capabilities
     ...generateMetricsCapabilities('MicrosoftEdge', '80', ['macOS 10.12', 'Windows 10', 'OS X 10.10'], ['INP', 'TTFB', 'FCP']), // done1
     ...generateMetricsCapabilities('chrome', '78', ['macOS 10.12', 'OS X 10.10'], ['FID', 'INP', 'TTFB', 'FCP']), // done2
@@ -34,15 +37,15 @@ exports.config = {
     ...generateMetricsCapabilities('MicrosoftEdge', '97', ['macOS 10.12', 'Windows 10'], ['LCP', 'FID', 'CLS', 'TTFB', 'FCP']),// done8
     ...generateMetricsCapabilities('chrome', '97', ['macOS 10.12', 'Windows 7', 'OS X 10.11'], ['LCP', 'FID', 'CLS', 'TTFB', 'FCP']),// done9
     ...generateMetricsCapabilities('MicrosoftEdge', '79', ['macOS 10.12', 'OS X 10.10'], ['LCP', 'FID', 'CLS', 'INP']),// done10
-    ...generateCapabilities('MicrosoftEdge', '13', 'Windows 10', { excludes: ['LCP', 'FID', 'CLS', 'INP', 'FCP'] }), // done10-1
-    ...generateCapabilities('MicrosoftEdge', '92', 'Windows 10', { excludes: ['LCP', 'FID', 'CLS', 'INP', 'TTFB'] }), // done10-2
-    ...generateMetricsCapabilities('chrome', '44', ['macOS 10.12', 'OS X 10.10'], ['LCP', 'FID', 'CLS', 'INP', 'FCP']),// done11
+    ...generateCapabilities('MicrosoftEdge', '13', 'Windows 10', { excludes: excludeTTFB }), // done10-1
+    ...generateCapabilities('MicrosoftEdge', '92', 'Windows 10', { excludes: excludeFCP }), // done10-2
+    ...generateMetricsCapabilities('chrome', '44', ['macOS 10.12', 'OS X 10.10'], excludeTTFB),// done11
     ...generateCapabilities('chrome', '67', 'Windows 7', { excludes: ['LCP', 'FID', 'INP', 'TTFB'] }), // done11-1
-    ...generateMetricsCapabilities('firefox', '35', ['OS X 10.10', 'macOS 10.12', 'Windows 7'], ['LCP', 'FID', 'CLS', 'INP', 'FCP']),// done12
-    ...generateCapabilities('safari', '11', 'macOS 10.12', { excludes: ['LCP', 'FID', 'CLS', 'INP', 'FCP'] }),// done13
-    ...generateCapabilities('firefox', '84', 'macOS 10.12', { excludes: ['LCP', 'FID', 'CLS', 'INP', 'TTFB'] }), // done15
-    ...generateCapabilities('firefox', '85', 'Windows 7', { excludes: ['LCP', 'FID', 'CLS', 'INP', 'TTFB'] }), // done16
-    ...generateCapabilities('safari', '15', 'macOS 12', { excludes: ['LCP', 'FID', 'CLS', 'INP', 'TTFB'] }),// done17
+    ...generateMetricsCapabilities('firefox', '35', ['OS X 10.10', 'macOS 10.12', 'Windows 7'], excludeTTFB),// done12
+    ...generateCapabilities('safari', '11', 'macOS 10.12', { excludes: excludeTTFB }),// done13
+    ...generateCapabilities('firefox', '84', 'macOS 10.12', { excludes: excludeFCP }), // done15
+    ...generateCapabilities('firefox', '85', 'Windows 7', { excludes: excludeFCP }), // done16
+    ...generateCapabilities('safari', '15', 'macOS 12', { excludes: excludeFCP }),// done17
   ],
   // Do not allow parallel test execution. Makes the test execution a lot
   // slower, but the setup simpler.
@@ -52,20 +55,20 @@ exports.config = {
   }
 };
 
-const metrics = ['LCP', 'FID', 'CLS', 'INP', 'TTFB', 'FCP'];
 
 function generateCapabilities(browserName, version, platform, options = {}) {
-  const excludes = options.excludes || [];
-  const includes = metrics.filter(metric => !excludes.includes(metric));
+  const { excludes = [] } = options;
+  const includes = excludes.length === 0
+    ? []
+    : metrics.filter(metric => !excludes.includes(metric));
 
   if (includes.length === 0) {
-    // If no metrics are included, return the default capability for e2e tests
     return [newSaucelabsCapability(browserName, version, platform)];
   }
 
-  // Create capabilities for each included metric
   return includes.map(metric => newSaucelabsCapability(browserName, version, platform, metric));
 }
+
 
 function generateMetricsCapabilities(browserName, version, platforms, excludes) {
   return platforms.flatMap(platform => generateCapabilities(browserName, version, platform, { excludes }));

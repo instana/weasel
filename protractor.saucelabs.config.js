acf00539ea1e7700b4e911c584e4f226f981216b
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-06-27 16:26:09 +0530

    TEST

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -1,4 +1,6 @@
 /* eslint-env node */
+const webVitalMetrics = ['LCP', 'FID', 'CLS', 'INP', 'TTFB', 'FCP'];
+const excludeTTFB = ['LCP', 'FID', 'CLS', 'INP', 'FCP'];
 
 exports.config = {
   // specs: ['test/e2e/**/*.spec.js'],
@@ -9,17 +11,17 @@ exports.config = {
   sauceBuild: process.env.GITHUB_RUN_NUMBER,
   // See https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
   multiCapabilities: [
-    newSaucelabsCapability('internet explorer', '11.103', 'Windows 10'),
-    newSaucelabsCapability('MicrosoftEdge', '14.14393', 'Windows 10'),
-    newSaucelabsCapability('safari', '9.0', 'OS X 10.11'),
-    newSaucelabsCapability('safari', '10.1', 'macOS 10.12'),
-    newSaucelabsCapability('safari', '11.0', 'macOS 10.12'),
-    newSaucelabsCapability('safari', '11.1', 'macOS 10.13'),
-    newSaucelabsCapability('firefox', '78.0', 'Windows 7'),
-    newSaucelabsCapability('firefox', '58.0', 'Windows 11'),
-    newSaucelabsCapability('chrome', '67.0', 'Windows 10'),
-    newSaucelabsCapability('chrome', '54.0', 'OS X 10.11'),
-    newSaucelabsCapability('chrome', '65.0', 'OS X 10.11')
+    newSaucelabsCapability('internet explorer', '11.103', 'Windows 10', { excludes: webVitalMetrics }),
+    newSaucelabsCapability('MicrosoftEdge', '14.14393', 'Windows 10', { excludes: excludeTTFB }),
+    // newSaucelabsCapability('safari', '9.0', 'OS X 10.11', { excludes: webVitalMetrics }),
+    // newSaucelabsCapability('safari', '10.1', 'macOS 10.12', { excludes: webVitalMetrics }),
+    // newSaucelabsCapability('safari', '11.0', 'macOS 10.12', { excludes: webVitalMetrics }),
+    // newSaucelabsCapability('safari', '11.1', 'macOS 10.13', { excludes: webVitalMetrics }),
+    // newSaucelabsCapability('firefox', '78.0', 'Windows 7', { excludes: webVitalMetrics }),
+    // newSaucelabsCapability('firefox', '58.0', 'Windows 11', { excludes: webVitalMetrics }),
+    // newSaucelabsCapability('chrome', '67.0', 'Windows 10', { excludes: webVitalMetrics }),
+    // newSaucelabsCapability('chrome', '54.0', 'OS X 10.11', { excludes: webVitalMetrics }),
+    // newSaucelabsCapability('chrome', '65.0', 'OS X 10.11', { excludes: webVitalMetrics }),
   ],
   // Do not allow parallel test execution. Makes the test execution a lot
   // slower, but the setup simpler.

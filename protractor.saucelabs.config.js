c9e7628268aa0eef64023e89308c385f07866b25
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-07-02 18:41:53 +0530

    TEST all

--- protractor.saucelabs.config.js
+++ protractor.saucelabs.config.js
@@ -17,30 +17,30 @@ exports.config = {
   // (e.g. Chrome, Edge, Opera, Samsung Internet). - https://github.com/GoogleChrome/web-vitals -
   multiCapabilities: [
     ...generateSauceLabsCapabilities('internet explorer', '11.103', ['Windows 10'], { includedMetrics: [] }),
-    // ...generateSauceLabsCapabilities('MicrosoftEdge', '14.14393', ['Windows 10'], { includedMetrics: testTTFBmetrics }),
-    // ...generateSauceLabsCapabilities('safari', '9.0', ['OS X 10.11'], { includedMetrics: [] }),
-    // ...generateSauceLabsCapabilities('safari', '10.1', ['macOS 10.12'], { includedMetrics: [] }),
-    // ...generateSauceLabsCapabilities('safari', '11.0', ['macOS 10.12'], { includedMetrics: testTTFBmetrics }),
-    // ...generateSauceLabsCapabilities('safari', '11.1', ['macOS 10.13'], { includedMetrics: testTTFBmetrics }),
-    // ...generateSauceLabsCapabilities('firefox', '78.0', ['Windows 7'], { includedMetrics: testTTFBmetrics }),
-    // ...generateSauceLabsCapabilities('firefox', '58.0', ['Windows 11'], { includedMetrics: testTTFBmetrics }),
-    // ...generateSauceLabsCapabilities('chrome', '67.0', ['Windows 10'], { includedMetrics: testTTFBmetrics }),
-    // ...generateSauceLabsCapabilities('chrome', '54.0', ['OS X 10.11'], { includedMetrics: testTTFBmetrics }),
-    // ...generateSauceLabsCapabilities('chrome', '65.0', ['OS X 10.11'], { includedMetrics: testTTFBmetrics }),
+    ...generateSauceLabsCapabilities('MicrosoftEdge', '14.14393', ['Windows 10'], { includedMetrics: testTTFBmetrics }),
+    ...generateSauceLabsCapabilities('safari', '9.0', ['OS X 10.11'], { includedMetrics: [] }),
+    ...generateSauceLabsCapabilities('safari', '10.1', ['macOS 10.12'], { includedMetrics: [] }),
+    ...generateSauceLabsCapabilities('safari', '11.0', ['macOS 10.12'], { includedMetrics: testTTFBmetrics }),
+    ...generateSauceLabsCapabilities('safari', '11.1', ['macOS 10.13'], { includedMetrics: testTTFBmetrics }),
+    ...generateSauceLabsCapabilities('firefox', '78.0', ['Windows 7'], { includedMetrics: testTTFBmetrics }),
+    ...generateSauceLabsCapabilities('firefox', '58.0', ['Windows 11'], { includedMetrics: testTTFBmetrics }),
+    ...generateSauceLabsCapabilities('chrome', '67.0', ['Windows 10'], { includedMetrics: testTTFBmetrics }),
+    ...generateSauceLabsCapabilities('chrome', '54.0', ['OS X 10.11'], { includedMetrics: testTTFBmetrics }),
+    ...generateSauceLabsCapabilities('chrome', '65.0', ['OS X 10.11'], { includedMetrics: testTTFBmetrics }),
 
-    // ...generateSauceLabsCapabilities('chrome', '96', ['macOS 10.12', 'Windows 7'], { includedMetrics: webvitalMetrics }),
-    // ...generateSauceLabsCapabilities('chrome', '97', ['macOS 10.12', 'Windows 7', 'OS X 10.11'], { includedMetrics: ['INP'] }),
-    // ...generateSauceLabsCapabilities('chrome', '85', ['OS X 10.10'], { includedMetrics: testFCPmetrics }),
-    // ...generateSauceLabsCapabilities('chrome', '125', ['macOS 11'], { includedMetrics: webvitalMetrics }),
-    // ...generateSauceLabsCapabilities('firefox', '35', ['OS X 10.10'], { includedMetrics: ['TTFB'] }),
-    // ...generateSauceLabsCapabilities('firefox', '84', ['macOS 10.12'], { testFCPmetrics: testFCPmetrics }),
-    // ...generateSauceLabsCapabilities('firefox', '85', ['Windows 7'], { testFCPmetrics: testFCPmetrics }),
-    // ...generateSauceLabsCapabilities('firefox', '90', ['macOS 10.12', 'Windows 7'], { includedMetrics: ['FID'] }),
-    // ...generateSauceLabsCapabilities('firefox', '122', ['macOS 10.15', 'Windows 10'], { includedMetrics: fireFox_supporting_wv_metrices }),
-    // ...generateSauceLabsCapabilities('MicrosoftEdge', '80', ['macOS 10.12', 'Windows 10', 'OS X 10.10'], { includedMetrics: ['LCP', 'FID', 'CLS', 'TTFB', 'FCP'] }),
-    // ...generateSauceLabsCapabilities('MicrosoftEdge', '97', ['macOS 10.12', 'Windows 10'], { includedMetrics: ['INP'] }),
-    // ...generateSauceLabsCapabilities('MicrosoftEdge', '79', ['macOS 10.12', 'OS X 10.10'], { includedMetrics: ['TTFB', 'FCP'] }),
-    // ...generateSauceLabsCapabilities('safari', '15', ['macOS 12'], { testFCPmetrics: testFCPmetrics }),
+    ...generateSauceLabsCapabilities('chrome', '85', ['OS X 10.10'], { includedMetrics: testFCPmetrics }),
+    ...generateSauceLabsCapabilities('chrome', '96', ['macOS 10.12', 'Windows 7'], { includedMetrics: webvitalMetrics }),
+    ...generateSauceLabsCapabilities('chrome', '97', ['macOS 10.12', 'Windows 7', 'OS X 10.11'], { includedMetrics: ['INP'] }),
+    ...generateSauceLabsCapabilities('chrome', '125', ['macOS 11'], { includedMetrics: webvitalMetrics }),
+    ...generateSauceLabsCapabilities('firefox', '35', ['OS X 10.10'], { includedMetrics: ['TTFB'] }),
+    ...generateSauceLabsCapabilities('firefox', '84', ['macOS 10.12'], { testFCPmetrics: testFCPmetrics }),
+    ...generateSauceLabsCapabilities('firefox', '85', ['Windows 7'], { testFCPmetrics: testFCPmetrics }),
+    ...generateSauceLabsCapabilities('firefox', '90', ['macOS 10.12', 'Windows 7'], { includedMetrics: ['FID'] }),
+    ...generateSauceLabsCapabilities('firefox', '122', ['macOS 10.15', 'Windows 10'], { includedMetrics: fireFox_supporting_wv_metrices }),
+    ...generateSauceLabsCapabilities('MicrosoftEdge', '80', ['macOS 10.12', 'Windows 10', 'OS X 10.10'], { includedMetrics: ['LCP', 'FID', 'CLS', 'TTFB', 'FCP'] }),
+    ...generateSauceLabsCapabilities('MicrosoftEdge', '97', ['macOS 10.12', 'Windows 10'], { includedMetrics: ['INP'] }),
+    ...generateSauceLabsCapabilities('MicrosoftEdge', '79', ['macOS 10.12', 'OS X 10.10'], { includedMetrics: ['TTFB', 'FCP'] }),
+    ...generateSauceLabsCapabilities('safari', '15', ['macOS 12'], { testFCPmetrics: testFCPmetrics }),
   ],
   // Do not allow parallel test execution. Makes the test execution a lot
   // slower, but the setup simpler.
@@ -52,7 +52,7 @@ exports.config = {
 
 // Filter out the metrics that are in the excludes array, and generate capabilities for each platform.
 // Note that not all web vital metrics are supported across all compatibilities.
-function generateSauceLabsCapabilities(browserName, version, platforms, options) {
+function generateSauceLabsCapabilities(browserName, version, platforms, options = []) {
   // const includedMetrics = webvitalMetrics.filter(metric => !options.excludes.includes(metric));
   return platforms.flatMap(platform => {
     return newSaucelabsCapability(browserName, version, platform, options.includedMetrics);

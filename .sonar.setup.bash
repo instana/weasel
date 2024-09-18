cd52016bafe760c8f1832ca37970c3f32f164c98
rgreene <rgreene@us.ibm.com>
2024-09-18 16:08:40 -0500

    Try original sonar.setup.bash while having a properties file

--- .sonar.setup.bash
+++ .sonar.setup.bash
@@ -5,11 +5,4 @@ set -eo pipefail
 yarn install --immutable
 
 # create unit-test coverage report - fail on any errors
-nvm install
-nvm use
-nvm version
-
-corepack enable
-
-yarn
-yarn test:quick
+yarn coverage

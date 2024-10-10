6d85ac65dcf5b88185b36f3fa2e69b4d5532f1d3
Argho Roy <Argho@ibm.com>
2024-10-10 14:13:10 -0400

    Remove backend trace id for cached responses

--- lib/resources/timingSerializer.ts
+++ lib/resources/timingSerializer.ts
@@ -10,12 +10,14 @@ export function serializeEntryToArray(entry: PerformanceResourceTiming) {
 
   // When timing data is available, we can provide additional information about
   // caching and resource sizes.
+  let isCached = false;
   if (typeof entry['transferSize'] === 'number' &&
       typeof entry['encodedBodySize'] === 'number' &&
       // All this information may not be available due to the timing allow origin check.
       entry['encodedBodySize'] > 0) {
     if (entry['transferSize'] === 0) {
       result.push(cachingTypes.cached);
+      isCached = true;
     } else if (entry['transferSize'] > 0 && (entry['encodedBodySize'] === 0 || entry['transferSize'] < entry['encodedBodySize'])) {
       result.push(cachingTypes.validated);
     } else {
@@ -68,19 +70,21 @@ export function serializeEntryToArray(entry: PerformanceResourceTiming) {
   }
 
   let backendTraceId = '';
-  try {
-    const serverTimings = entry['serverTiming'];
-    if (serverTimings instanceof Array) {
-      for (let i = 0; i < serverTimings.length; i++) {
-        const serverTiming = serverTimings[i];
-        if (serverTiming['name'] === vars.serverTimingBackendTraceIdEntryName) {
-          backendTraceId = serverTiming['description'];
+  if (!isCached){
+    try {
+      const serverTimings = entry['serverTiming'];
+      if (serverTimings instanceof Array) {
+        for (let i = 0; i < serverTimings.length; i++) {
+          const serverTiming = serverTimings[i];
+          if (serverTiming['name'] === vars.serverTimingBackendTraceIdEntryName) {
+            backendTraceId = serverTiming['description'];
+          }
         }
       }
+    } catch (e) {
+      // Some browsers may not grant access to the field when the Timing-Allow-Origin
+      // check fails. Better be safe than sorry here.
     }
-  } catch (e) {
-    // Some browsers may not grant access to the field when the Timing-Allow-Origin
-    // check fails. Better be safe than sorry here.
   }
   result.push(backendTraceId);
 

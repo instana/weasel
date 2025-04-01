e4b8ff0aa3b559060bbfd548b5bd4eddfdf424b3
Joby Korah George <jobykorahgeorge@ibm.com>
2025-04-01 13:59:38 +0530

    INSTA-31599: pageload trace from server-initiated trace (#139)
    * pageload trace from server initiated trace
    
    * updated the referene docuemnt link
    
    * updating validator

--- lib/states/init.ts
+++ lib/states/init.ts
@@ -72,6 +72,14 @@ const state: State = {
 
     // prefer the backend trace ID which was explicitly set
     vars.pageLoadBackendTraceId = vars.pageLoadBackendTraceId || getPageLoadBackendTraceId();
+
+    // If the server is not monitored by Instana, the backend trace ID for the page load will be 
+    // extracted from the traceparent injected by webserver service.
+    // Reference: https://opentelemetry.io/docs/languages/js/getting-started/browser/ 
+    const traceState = checkPageLoadTraceParentAvalable();
+    if (vars.enableW3CHeaders && traceState) {
+      vars.pageLoadBackendTraceId = traceState;
+    }
     vars.initializerExecutionTimestamp = globalObject['l'];
 
     addCommandAfterInitializationSupport();
@@ -120,4 +128,37 @@ function addCommandAfterInitializationSupport() {
 
 function fulfillsPrerequisites() {
   return win.XMLHttpRequest && win.JSON;
+}
+
+// When a page request is made, the Instana JS SDK isn't initialized until the server returns the HTML and the app's 
+// JS assets. To pass the trace from the server to the client, the trace ID is sent in a <meta> tag. 
+/*
+  Example: 
+    <meta
+      name="traceparent"
+      content="00-ab42124a3c573678d4d8b21ba52df3bf-d21f7bc17caa5aba-01"
+    />
+*/
+function checkPageLoadTraceParentAvalable() : string | null {
+  const traceParentMeta = document.querySelector('meta[name="traceparent"]') as HTMLMetaElement;
+  
+  // Ensure that traceparent exists and is in the correct format
+  if (!traceParentMeta || !traceParentMeta.content) {
+    return null;
+  }
+
+  const traceParent = traceParentMeta.content;
+
+  // Validity check: traceparent must have 3 parts and each part must have a valid length
+  const traceParentParts = traceParent.split('-');
+  if (traceParentParts.length === 4 && 
+      traceParentParts[0] === '00' && // Ensure version is '00'
+      traceParentParts[1].length === 32 && // Ensure trace ID length is 32
+      traceParentParts[2].length === 16 && // Ensure span ID length is 16
+      traceParentParts[3].length === 2) { // Ensure flags length is 2
+    return traceParentParts[2]; // Return the tracestate
+  }
+
+  // If any part is invalid, return null
+  return null;
 }
